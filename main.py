aefa3dbd7dee04d06b172cf12811718025c28cbf
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2023-09-09 16:01:25 -0300

    indo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math
+
+np.seterr(divide='ignore', invalid='ignore')
+
 """
 Problema de programação linear de exemplo
  min z = x1 - 2x2 + x3
@@ -35,24 +38,35 @@ b = np.array([9, 15])
 c = np.array([-1, -9, -1])
 """
 
-A = np.array([[1, 2, 3], [3, 2, 2]])
-b = np.array([9, 15])
-c = np.array([-1, -9, -1])
+tipo = ('min', 'max')
+A = np.array([[1, 2, 4, -1], [2, 3, -1, 1], [1, 0, 1, 1]])
+b = np.array([6, 12, 4])
+c = np.array([2, 1, -3, 5])
+
+problema = str(input('O problema é de MAX ou MIN? '))
+
+while True:
+    if problema.lower() in tipo:
+        if problema.lower() == 'max':
+            c = np.dot(c, -1)
+            break
+        elif problema.lower() == 'min':
+            break
+    else:
+        problema = str(input('Opção incorreta...MAX ou MIN? '))
 
 def simplex(A, b, c):
 
     m, n = A.shape #"m" número de restrições, "n" número de variáveis
     A = np.hstack([A, np.eye(m, dtype=int)]) #cria matriz identidade das variáveis de folga
     c = np.hstack([c, np.zeros(m)]) # "c" contém os custos
-
     var = np.arange(n + m) # var = [0, 1, ..., n + m - 1] -- portanto nao ficara na ordem correta... realizar a correcao posteiormente
-
     vb = var[n:] # vb = [n, n + 1, ..., n + m - 1] ('vb' variáveis da base)
     vnb = var[:n] # vnb = [0, 1, ..., n - 1] ('vnb' variáveis não base)
 
     while True:
 
-        B = np.linalg.inv (A[:, vb]) #cálculo da inversa de B -  A[:, vb] contém a matriz dos elementos da base...
+        B = np.linalg.inv(A[:, vb]) #cálculo da inversa de B -  A[:, vb] contém a matriz dos elementos da base...
 
         #cálculo da solução básica factível
         sbf = np.dot(B, b)
@@ -64,7 +78,7 @@ def simplex(A, b, c):
         # Verificar se a solução atual é ótima
         # Se todos os custos relativos são maiores ou iguais a zero, a solução é ótima
         if np.all(cnb >= 0): #Se todos os custos de cnb forem maior que 0 o laço é encerrado.
-            print('Solução ótima encontrada!!!\n')
+            print('\nSolução ótima encontrada!!!\n')
             break
 
         # Escolha da variável que entra na base
@@ -74,12 +88,7 @@ def simplex(A, b, c):
 
         #Teste de razão
         y = np.dot(B, A[:, k])
-
-        # Calcular os coeficientes da equação da reta que sai da solução atual em direção à melhora da função objetivo¬
-        try:
-            yA = np.dot(B, b) / y
-        except ZeroDivisionError:
-            print(" ")
+        yA = np.dot(B, b) / y
 
         # Verificar se o problema tem solução limitada
         # Se todos os coeficientes da reta são menores ou iguais a zero, o problema é ilimitado
@@ -90,7 +99,6 @@ def simplex(A, b, c):
         #Escolha da variável que sai da base
         y_pos = yA[yA > 0] # y_pos é o vetor dos coeficientes positivos da reta -- tratar valores infinitos caso der tempo
         y_min = np.argmin(y_pos) # y_min é a posição da variável que sai da base no vetor y_pos (indice do valor mínimo)
-
 
         il = np.where(yA == y_pos[y_min])[0][0] # il é a posição da variável que sai da base no vetor
         xl = vb[il] # xl é o índice da variável que sai da base (vale a pena ressaltar [0...-n])
